622c95954b18c7ffb0f33bc3a572a553bd1cc2f0
FacuMartinGarcia <estudiodesarrolladorsoftware@gmail.com>
2025-04-03 20:06:15 -0300

    ..

--- src/pila/Pilas.java
+++ src/pila/Pilas.java
@@ -28,61 +28,61 @@ public class Pilas {
         pila hasta encontrar la posición correcta y volver a colocar los elementos
         retirados, solo puede usar una pila auxiliar.
         */
-        
-        Pila p1 = new Pila(5);
-        Pila p2 = new Pila(5); 
-
-        int ultimo=0; 
-        int ultimoMayores=0;
-        
-        while (true) {
-
-            String numero = JOptionPane.showInputDialog(null, "Ingrese un número (e):");
-            
-            
-            if (numero == null || numero.equalsIgnoreCase("e")) {
-                break;
-           
-            }
-            
-            int num = Integer.parseInt(numero); 
-
-            //si es menor que el ultimo de la pila
-
-            for (int i = p1.verTope; i = 0; i--) {
-                
-            }
-
-
-            
-            if (num>ultimo) {
-                p1.push(num); 
-            }else{
-                
-                
-                
-                //si es mayor que el utlimo de la pila, pero menor al ultimo de los mayores
-                if (num>ultimoMayores){
-                    p1.push(ultimoMayores);
-                    p2.pop();
-                }else{    
-                    p2.push(num);
-                }        
-            }
-
-        ultimo = p1.verTope();
-        if (p2.getTope() != -1){
-            ultimoMayores = p2.getTope();
-        }
-        System.out.println("Ultimo " + ultimo);
-        System.out.println("Ultimo mayores" + ultimoMayores);
-
-
-                
-        }
-        
-        p1.mostrarPila();
-        //p2.mostrarPila();
+//        
+//        Pila p1 = new Pila(5);
+//        Pila p2 = new Pila(5); 
+//
+//        int ultimo=0; 
+//        int ultimoMayores=0;
+//        
+//        while (true) {
+//
+//            String numero = JOptionPane.showInputDialog(null, "Ingrese un número (e):");
+//            
+//            
+//            if (numero == null || numero.equalsIgnoreCase("e")) {
+//                break;
+//           
+//            }
+//            
+//            int num = Integer.parseInt(numero); 
+//
+//            //si es menor que el ultimo de la pila
+//
+//            for (int i = p1.verTope; i = 0; i--) {
+//                
+//            }
+//
+//
+//            
+//            if (num>ultimo) {
+//                p1.push(num); 
+//            }else{
+//                
+//                
+//                
+//                //si es mayor que el utlimo de la pila, pero menor al ultimo de los mayores
+//                if (num>ultimoMayores){
+//                    p1.push(ultimoMayores);
+//                    p2.pop();
+//                }else{    
+//                    p2.push(num);
+//                }        
+//            }
+//
+//        ultimo = p1.verTope();
+//        if (p2.getTope() != -1){
+//            ultimoMayores = p2.getTope();
+//        }
+//        System.out.println("Ultimo " + ultimo);
+//        System.out.println("Ultimo mayores" + ultimoMayores);
+//
+//
+//                
+//        }
+//        
+//        p1.mostrarPila();
+//        //p2.mostrarPila();
 
     }
     
